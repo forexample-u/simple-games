c31a8f5c83311e3cddddcee9d86c214c9750021e
forexample-u <98162342+forexample-u@users.noreply.github.com>
2022-04-04 01:03:36 +0300

    Update main_menu.h

--- simple-games/main_menu.h
+++ simple-games/main_menu.h
@@ -1,27 +1,34 @@
 #pragma once
 #include "interface.h"
-#include "move.h"
+#include "settings_menu.h"
+#include "game_menu.h"
+#include "test_menu.h"
 
 
 void main_menu() {
+	Console cmd;
 	std::vector<std::string> list_menu{
-	"Start",
-	"Test",
-	"Settings",
-	"Quit"
+		"Start",
+		"Test",
+		"Settings",
+		"Quit"
 	};
-
 	std::string choice = MakeMenu::center_menu(list_menu, Color(0, 0), Color(0, 11), Color(0, 12), 5, 3, 4, 0.375);
+	
+	cmd.color_reset();
+	cmd.clear();
 	if (choice == "Start") {
-		//TODO: start menu
+		game_menu();
 	}
 	if (choice == "Test") {
-		//TODO: test menu
+		test_menu();
 	}
 	if (choice == "Settings") {
-		//TODO: settings menu
+		settings_menu();
 	}
 	if (choice == "Quit") {
+		cmd.color_reset();
+		cmd.clear();
 		return;
 	}
 }
--- simple-games/simple-games.cpp
+++ simple-games/simple-games.cpp
@@ -9,13 +9,4 @@
 
 int main(){
 	main_menu();
-
-	ping_pong();
-	breakout();
-
-	test::collide_test();
-	test::menu_test();
-	test::move_player();
-	test::button_auto_scale();
-	start_snake();
 }
