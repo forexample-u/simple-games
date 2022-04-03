dcac7be0871eb9f771eddcbb7f97f853aa18dcba
forexample-u <98162342+forexample-u@users.noreply.github.com>
2022-04-04 00:28:43 +0300

    Update create MakeMenu

--- simple-games/interface.h
+++ simple-games/interface.h
@@ -165,3 +165,68 @@ private:
 	int selected_index = 0;
 	std::vector<Button> buttons;
 };
+
+namespace MakeMenu {
+	//make_menu
+	std::string center_menu(std::vector<std::string> list_menu, Color color_bg = Color(0, 0), Color color_button = Color(0, 11),
+		Color color_selected = Color(0, 12), int pos_y = 5, int size_y = 3, int padding_y = 4, double scale_x = 0.375) {
+
+		Plane plane;
+		Move move;
+		Console cmd;
+		Menu menu;
+		Button button;
+
+		menu.set_selected_color(color_selected);
+		plane.set_color_bg(color_bg);
+		button.set_color_bg(color_bg);
+		button.set_color_button(color_button);
+		button.set_pos(Coord(0, pos_y));
+		Size screen_size = cmd.get_size_screen();
+		button.set_size(Size(screen_size.width * scale_x, size_y));
+		button.set_pos_center(1, 0);
+		for (const auto& button_word : list_menu) {
+			button.set_text(button_word);
+			menu.push_button(button);
+			button.add_pos(Coord(0, padding_y));
+		}
+		
+		plane.set_size(Size(0, 0));
+		plane.print_bg();
+		menu.print();
+		while (true) {
+			Size screen_size_now = cmd.get_size_screen();
+			if (abs(screen_size_now.width - screen_size.width) >= 5 || screen_size_now.width <= 20) {
+				cmd.clear();
+				cmd.color(color_bg);
+				for (int y = 0; y < screen_size_now.height; y++) {
+					cmd.gotoxy(0, y);
+					std::cout << '.';
+				}
+
+				for (size_t i = 0; i < menu.size(); i++) {
+					menu[i].set_size(Size(screen_size_now.width * scale_x, menu[i].get_size().height));
+					menu[i].set_pos_center(1, 0);
+					int pos_x = menu[i].get_pos().x - 4;
+					if (pos_x <= 0) { pos_x = 0; }
+					menu[i].set_pos(Coord(pos_x, menu[i].get_pos().y));
+				}
+				menu.print();
+				cmd.sleep(20);
+				screen_size = cmd.get_size_screen();
+			}
+
+			menu.move(move);
+			cmd.sleep(60);
+			if (move.now.get_button() != false) {
+				menu.print();
+			}
+			if (move.now.get_enter() || move.now.get_space()) {
+				menu.print();
+				break;
+			}
+		}
+		int index_menu = menu.get_selected_index();
+		return list_menu[index_menu];
+	}
+}
