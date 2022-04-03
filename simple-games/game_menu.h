575e0de8b66624ab06c7f82e5168e101b0861a06
forexample-u <98162342+forexample-u@users.noreply.github.com>
2022-04-04 00:56:35 +0300

    Add game_menu.h

--- simple-games/game_menu.h
+++ simple-games/game_menu.h
@@ -1,1 +1,27 @@
 #pragma once
+#include "PingPong.h"
+#include "Snake.h"
+#include "Breakout.h"
+#include "interface.h"
+
+void game_menu() {
+	Console cmd;
+	std::vector<std::string> list_games {
+		"Snake",
+		"Ping-pong",
+		"Breakout"
+	};
+	std::string choice = MakeMenu::center_menu(list_games, Color(0, 0), Color(0, 11), Color(0, 12), 5, 3, 4, 0.375);
+	
+	cmd.color_reset();
+	cmd.clear();
+	if (choice == "Snake") {
+		start_snake();
+	}
+	if (choice == "Ping-pong") {
+		ping_pong();
+	}
+	if (choice == "Breakout") {
+		breakout();
+	}
+}
