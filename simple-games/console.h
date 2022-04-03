df51db11e4555a5a03f335b3acedf0984c42ec20
forexample-u <98162342+forexample-u@users.noreply.github.com>
2022-04-03 19:23:15 +0300

    Update console.h add ansi

--- simple-games/console.h
+++ simple-games/console.h
@@ -6,18 +6,21 @@
 #include <Windows.h>
 #include "core.h"
 
+#define Ansi_test
+
 class Console {
 public:
-	void sleep(int ms) {
+	void sleep(int ms) const {
 		std::this_thread::sleep_for(std::chrono::milliseconds(ms));
-	}
-
-	void gotoxy(int x, int y) const {
-		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { short(x), short(y) });
 	}
 
 	void gotoxy(Coord pos) const {
 		gotoxy(pos.x, pos.y);
+	}
+
+#ifndef Ansi_test
+	void gotoxy(int x, int y) const {
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { short(x), short(y) });
 	}
 
 	void resize_screen(Size size) const {
@@ -48,11 +51,66 @@ public:
 	}
 
 	Size get_size_screen() const {
-		Size size_screen;
-		CONSOLE_SCREEN_BUFFER_INFO screen;
-		GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &screen);
-		size_screen.width = (screen.srWindow.Right - screen.srWindow.Left + 1);
-		size_screen.height = (screen.srWindow.Bottom - screen.srWindow.Top + 1);
-		return size_screen;
+		CONSOLE_SCREEN_BUFFER_INFO screen_info;
+		GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &screen_info);
+		auto screen = screen_info.srWindow;
+		return Size(screen.Right - screen.Left + 1, screen.Bottom - screen.Top + 1);
+	}
+#endif
+#ifdef Ansi_test
+	void gotoxy(int x, int y) const {
+		std::cout << "\033[" << (y + 1) << "d\033[" << (x + 1) << "G";
+	}
+
+	void resize_screen(Size size) const {
+		std::cout << "\033[8;" << size.height << ";" << size.width << "t";
+	}
+
+	//TODO:
+	//1. pause()
+	//2. get_size_screen()
+	void pause() const {
+		system("pause > nul");
+	}
+	Size get_size_screen() const {
+		CONSOLE_SCREEN_BUFFER_INFO screen_info;
+		GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &screen_info);
+		auto screen = screen_info.srWindow;
+		return Size(screen.Right - screen.Left + 1, screen.Bottom - screen.Top + 1);
+	}
+
+	void clear() const {
+		std::cout << "\033c";
+	}
+
+	void color_font(int font_bit) const {
+		int font = ansi_color_to_windows_color(font_bit);
+		std::cout << "\033[38;5;" << font << "m";
+	}
+
+	void color(Color color) const {
+		int font = ansi_color_to_windows_color(color.get_font());
+		int bg = ansi_color_to_windows_color(color.get_bg());
+		std::cout << "\033[38;5;" << font << "m";
+		std::cout << "\033[48;5;" << bg << "m";
+	}
+
+	void color_reset() const {
+		std::cout << "\033[0m";
+	}
+#endif
+private:
+	//ansi color convert to windows color
+	int ansi_color_to_windows_color(int color_bit) const {
+		int color = color_bit;
+		if (color_bit == 1) { color = 4; };
+		if (color_bit == 4) { color = 1; };
+		if (color_bit == 3) { color = 6; };
+		if (color_bit == 6) { color = 3; };
+		if (color_bit == 1 + 8) { color = 4 + 8; };
+		if (color_bit == 4 + 8) { color = 1 + 8; };
+		if (color_bit == 3 + 8) { color = 6 + 8; };
+		if (color_bit == 6 + 8) { color = 3 + 8; };
+		return color;
 	}
 };
