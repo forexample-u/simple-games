b0e3e2e556e898f0ba66d8d6e65d4bd3358c23e4
forexample-u <98162342+forexample-u@users.noreply.github.com>
2022-04-05 21:19:14 +0300

    Fix Breakout.h (spawn inside border)

--- simple-games/Breakout.h
+++ simple-games/Breakout.h
@@ -6,13 +6,13 @@
 #include "move.h"
 #include "shape.h"
 #include "collider.h"
-
+#include <ctime>
 
 class Ball : public Collider {
 public:
 	//print
 	void print() {
-		std::string row(size_ball.width, trail_char);
+		std::string row(size_ball.width, char_ball);
 
 		cmd.color(color_bg);
 		for (int y = 0; y < size_ball.height; y++) {
@@ -22,6 +22,7 @@ public:
 
 		pos_ball.x += dir_ball.x;
 		pos_ball.y += dir_ball.y;
+
 		cmd.color(color_ball);
 		for (int y = 0; y < size_ball.height; y++) {
 			cmd.gotoxy(pos_ball.x, pos_ball.y + y);
@@ -32,10 +33,10 @@ public:
 	void detect_collision(Coord collision_pos, Size collision_size) {
 		collide.create(collision_size, collision_pos, size_ball, pos_ball);
 		if (collide.get_bounce().x != 0) {
-			set_dir(Dir(collide.get_bounce().x, get_dir().y));
+			set_dir(Dir(collide.get_bounce().x, dir_ball.y));
 		}
 		if (collide.get_bounce().y != 0) {
-			set_dir(Dir(get_dir().x, collide.get_bounce().y));
+			set_dir(Dir(dir_ball.x, collide.get_bounce().y));
 		}
 	}
 
@@ -60,8 +61,8 @@ public:
 		color_bg = new_color;
 	}
 
-	void set_trail_char(char new_ch) {
-		trail_char = new_ch;
+	void set_symbol_ball(char new_ch) {
+		char_ball = new_ch;
 	}
 
 	//get
@@ -78,12 +79,12 @@ public:
 	}
 
 private:
-	Color color_ball = Color(0, 15);
-	Color color_bg;
 	Coord pos_ball;
 	Size size_ball = Size(1, 1);
 	Dir dir_ball = Dir(1, 1);
-	char trail_char = ' ';
+	Color color_ball = Color(0, 15);
+	Color color_bg;
+	char char_ball = ' ';
 	Console cmd;
 	std::vector<Block> collisions;
 };
@@ -96,8 +97,8 @@ public:
 		dir_board = Dir(0, 0);
 		dir_board.x = move.now.get_dir_x();
 		dir_board.y = move.now.get_dir_y();
-		pos_board.x += dir_board.x * move_padding.x;
-		pos_board.y -= dir_board.y * move_padding.y;
+		pos_board.x += dir_board.x * move_step_board.x;
+		pos_board.y -= dir_board.y * move_step_board.y;
 	}
 
 	void border(Coord border_pos, Size border_size) {
@@ -121,26 +122,27 @@ public:
 		pos_board = new_pos;
 	}
 
-	void set_step(Coord step = Coord(3, 0)) {
-		move_padding = step;
-	}
-
 	void set_size(Size new_size) {
 		size_board = new_size;
+	}
+
+	void set_step(Coord new_step) {
+		move_step_board = new_step;
 	}
 
 	void set_symbol_board(char new_ch) {
 		char_board = new_ch;
 	}
 
-	void set_color_bg(Color new_color) {
-		color_bg = new_color;
-	}
-
 	void set_color_board(Color new_color) {
 		color_board = new_color;
 	}
 
+	void set_color_bg(Color new_color) {
+		color_bg = new_color;
+	}
+
+	//print
 	void print() const {
 		std::string row(size_board.width, char_board);
 
@@ -158,6 +160,7 @@ public:
 			std::cout << row;
 		}
 	}
+
 	//get
 	Coord get_pos() const {
 		return pos_board;
@@ -173,10 +176,10 @@ private:
 	Size size_board = Size(10, 1);
 	Dir dir_board = Dir(1, 0);
 	Color color_board = Color(7, 7);
-	Color color_bg;
-	Coord move_padding = Coord(3, 0);
+	Coord move_step_board = Coord(3, 0);
 	char char_board = ' ';
 
+	Color color_bg;
 	Console cmd;
 };
 
@@ -190,7 +193,7 @@ void breakout() {
 	Ball ball;
 	Block block;
 	Board player;
-	cmd.resize_screen(Size(220, 55));
+	cmd.resize_screen(Size(120, 30));
 
 	//color
 	plane.set_color_bg(Color(15, 15));
@@ -201,72 +204,77 @@ void breakout() {
 	block.set_color_block(Color(7, 7));
 	player.set_color_board(Color(12, 12));
 
+	//settings:
+	Size player_size = Size(5, 1);
+	Coord plane_pos = Coord(0, 0);
+	Size plane_size = Size(118, 29);
+	Size block_count = Size(10, 3);
+	Size block_size = Size(8, 2);
+	Size block_padding = Size(2, 1);
+	Coord block_offset = Coord(10, 2);
+
+	//calc settings
+	block_padding.width += block_size.width;
+	block_padding.height += block_size.height;
+
 	//plane
-	plane.set_pos(Coord(10, 3));
-	plane.set_size(Size(201, 50));
-	plane.print();
+	plane.set_pos(plane_pos);
+	plane.set_size(plane_size);
 
 	//player
-	player.set_size(Size(5, 2));
-	player.set_pos(Coord(rand() % 60, plane.get_size().height - player.get_size().height + 2));
+	player.set_pos(Coord(plane_pos.x + (rand() % (plane_size.width - 1)) + 1, plane_size.height - player_size.height - 1));
+	player.set_size(player_size);
 
 	//ball
-	ball.set_dir(Dir(1, -1));
-	ball.set_pos(Coord(player.get_pos().x, player.get_pos().y - player.get_size().height));
+	ball.set_pos(Coord(player.get_pos().x, player.get_pos().y - player_size.height - 2));
 	ball.set_size(Size(2, 1));
-
-	//block
-	block.set_pos(Coord(12, 12));
-	block.set_size(Size(16, 3));
+	ball.set_dir(Dir(1, -1));
 
 	//blocks
+	block.set_size(block_size);
 	std::vector<Block> blocks;
-	blocks.assign(10, block);
-	size_t size = blocks.size();
-	for (size_t i = 0; i < size; i++) {
-		blocks[i].add_pos(Coord(i * 20, 0));
-	}
-	for (size_t i = 0; i < 3; i++) {
-		for (size_t j = 0; j < size; j++) {
-			block = blocks[j + (i * size)];
-			block.add_pos(Coord(0, 5));
+	for (size_t y = 0; y < block_count.height; y++) {
+		block.set_pos(block_offset);
+		block.add_pos(Coord(0, block_padding.height * y));
+		for (size_t x = 0; x < block_count.width; x++) {
 			blocks.push_back(block);
+			block.add_pos(Coord(block_padding.width, 0));
 		}
 	}
+
+	plane.print();
 	for (const auto& block : blocks) {
 		block.print();
 	}
 
+	auto clock_start = std::clock();
+	int sleep_ball_ms = 1000;
 	while (true) {
-		ball.detect_collision(plane.get_pos(), plane.get_size());
 		for (size_t i = 0; i < blocks.size(); i++) {
 			ball.detect_collision(blocks[i].get_pos(), blocks[i].get_size());
 			if (ball.collide.is_any()) {
-				blocks[i].set_color_block(Color(0, 0));
+				blocks[i].set_color_block(plane.get_color_plane());
 				blocks[i].print();
 				blocks.erase(blocks.begin() + i);
 			}
 		}
 		ball.detect_collision(player.get_pos(), player.get_size());
-		
+		ball.detect_collision(plane.get_pos(), plane.get_size());
 
 		player.move(move);
-		//player.collision(plane.get_pos(), player.get_pos());
-		if (player.get_pos().x <= plane.get_pos().x + 1) {
-			player.set_pos(Coord(plane.get_pos().x + 1, player.get_pos().y));
-		}
-		if ((player.get_pos().x + player.get_size().width) > plane.get_pos().x + plane.get_size().width - 1) {
-			player.set_pos(Coord(plane.get_pos().x + plane.get_size().width - player.get_size().width - 1, player.get_pos().y));
-		}
-
-		ball.print();
+		player.border(plane);
 		player.print();
 
+		auto clock_now = std::clock();
+		if ((clock_now - clock_start) > sleep_ball_ms) {
+			ball.print();
+		}
+
 		cmd.sleep(50);
-		if (ball.get_pos().y >= plane.get_size().height) { // die
+		if (ball.get_pos().y >= plane_size.height - 2) { // die
 			break;
 		}
-		if (blocks.size() == 0) { // win
+		if (blocks.empty()) { // win
 			break;
 		}
 		if (move.now.get_escape()) {
