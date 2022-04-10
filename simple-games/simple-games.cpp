0ef4ac0782b882bf9d2f69a35946e18ef4c0f610
forexample-u <98162342+forexample-u@users.noreply.github.com>
2022-04-10 23:28:44 +0300

    Add start_menu.h

--- simple-games/simple-games.cpp
+++ simple-games/simple-games.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include "main_menu.h"
+#include "start_menu.h"
 
-
-int main(){
-	ListMenu::main_menu();
+int main() {
+	ListMenu::start_menu();
+	while (true) {
+		ListMenu::main_menu();
+	}
 }
---
+++ simple-games/start_menu.h
@@ -0,0 +1,69 @@
+#pragma once
+#include <string>
+#include "console.h"
+#include "shape.h"
+#include "main_menu.h"
+
+namespace ListMenu {
+	void start_menu() {
+		Move move;
+		Console cmd;
+		Plane plane;
+
+		//settings
+		int color_text = 15;
+		Color color_ball = Color(0, 0);
+		Color color_bg = Color(15, 15);
+		int center_pos_y = 24;
+
+		//print bg
+		cmd.resize_small_screen(Size(80, 24));
+		cmd.sleep(100);
+		plane.set_size(Size(0, 0));
+		plane.set_color_bg(color_bg);
+		plane.print_bg();
+		Size screen = cmd.get_size_screen();
+
+		//text
+		std::string label = "by @for_example";
+		int length_label = static_cast<int>(label.length());
+		Coord center_text = Coord(screen.width / 2 - length_label / 2 - 2, center_pos_y / 2);
+
+		//bounce ball
+		Ball ball;
+		ball.set_pos(Coord(center_text.x - 31, 5));
+		ball.set_color_bg(color_bg);
+		ball.set_color_ball(color_ball);
+		while (true) {
+			if (ball.get_pos().y >= center_pos_y - 1) {
+				ball.set_dir(Dir(1, -1));
+			}
+			if (ball.get_pos().y < center_pos_y / 2 + 2) {
+				if (ball.get_pos().x >= screen.width / 2 - length_label - 2) {
+					ball.print();
+					cmd.sleep(100);
+					break;
+				}
+				ball.set_dir(Dir(1, 1));
+			}
+			ball.print();
+			cmd.sleep(50);
+		}
+		plane.set_color_bg(color_ball);
+		plane.print_bg();
+		ball.print();
+		cmd.gotoxy(ball.get_pos().x - 1, ball.get_pos().y + 1);
+		cmd.sleep(1000);
+		cmd.color(Color(color_text, 0));
+		cmd.gotoxy(center_text);
+		for (const auto& ch : label) {
+			std::cout << ch;
+			cmd.sleep(100);
+		}
+		std::cout << ' ' << std::flush;
+		cmd.sleep(2100);
+		plane.print_bg();
+		cmd.sleep(900);
+		ListMenu::main_menu();
+	}
+}
